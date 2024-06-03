d65d6d9b43613c35d87437e45444d8e34cbf4bc2
Lorenzo Pollastrini <88330570+lorenzopollastrini@users.noreply.github.com>
2024-06-03 10:19:47 +0200

    Add support for arbitrary domains

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function normalize(data) {
+    let xValues = data.map(d => d.x);
+    let minXValue = d3.min(xValues);
+    let maxXValue = d3.max(xValues);
+
+    const normalizeX = d3.scaleLinear()
+        .domain([minXValue, maxXValue])
+        .range([0, 10]);
+
+    let yValues = data.map(d => d.y);
+    let minYValue = d3.min(yValues);
+    let maxYValue = d3.max(yValues);
+
+    const normalizeY = d3.scaleLinear()
+        .domain([minYValue, maxYValue])
+        .range([0, 10]);
+
+    let zValues = [];
+    data.forEach(d => {
+        zValues.push(d.z0, d.z1, d.z2);
+    });
+    let minZValue = d3.min(zValues);
+    let maxZValue = d3.max(zValues);
+
+    const normalizeZ = d3.scaleLinear()
+        .domain([minZValue, maxZValue])
+        .range([0.5, 1]);
+
+    data.forEach(d => {
+        d.x = normalizeX(d.x);
+        d.y = normalizeY(d.y);
+        d.z0 = normalizeZ(d.z0);
+        d.z1 = normalizeZ(d.z1);
+        d.z2 = normalizeZ(d.z2);
+    });
+}
+
 function join(data) {
     chart.selectAll(".butterfly")
         .data(data)
@@ -39,7 +76,8 @@ function join(data) {
                     "219.176 130.501 218.453 133.305 218.453 136.283 L 218.453 177.913 L 218.453 227.688 L 218.453 " +
                     "372.214 C 218.453 382.375 226.69 390.612 236.851 390.612 C 247.012 390.612 255.248 382.375 " +
                     "255.248 372.214 L 255.248 227.688 L 255.248 177.913 L 255.248 136.283 C 255.248 133.305 254.525 " +
-                    "130.5 253.269 128.012 C 258 116.983 264.629 107.371 273.398 99.145 Z");
+                    "130.5 253.269 128.012 C 258 116.983 264.629 107.371 273.398 99.145 Z")
+                .attr("fill", "rgb(205, 127, 50)");
         });
 }
 
@@ -89,18 +127,23 @@ const chart = d3.select("#chart-container").append("svg")
     .attr("height", chartHeight);
 
 // x-axis.
-chart.append("g")
+const xAxis = chart.append("g")
     .attr("transform", `translate(0, ${chartHeight - chartMarginBottom})`)
     .call(d3.axisBottom(x));
+xAxis.raise();
 
 // y-axis.
-chart.append("g")
+const yAxis = chart.append("g")
     .attr("transform", `translate(${chartMarginLeft}, 0)`)
     .call(d3.axisLeft(y));
 
 d3.json("butterflies.json")
     .then(function(data) {
+        normalize(data);
         join(data);
+
+        xAxis.raise();
+        yAxis.raise();
     })
     .catch(function(error) {
         console.log(error);
